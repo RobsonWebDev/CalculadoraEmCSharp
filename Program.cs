94c71562de873bc89571a899b49d3cdf0a5d8f89
Robson Cerqueira <84989456+RobsonWebDev@users.noreply.github.com>
2023-03-21 13:39:11 -0300

    Calculadora Básica
    Objetivo de fazer uma Calculadora básica em C# que me deu bastante conhecimento na linguagem pois tive que lê o código varias veze e tive que fazer algumas pesquisas por causa do erro que estava apresentando, kkk, mas saiu.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,165 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculadoraEmCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+
+            Console.WriteLine("-=-=-=Calculadora=-=-=-");
+            Console.WriteLine("-=-=-=-Versão 1.0=-=-=-");
+
+            Menu();
+
+            Console.Clear();
+
+            Console.ReadKey();
+        }
+
+        static void Menu()
+        {
+            //Console.Clear();
+            Console.Beep();
+
+            int opcao;
+
+            Console.WriteLine("-=-Digite uma Opção=-=-");
+            Console.WriteLine("Soma................[1]");
+            Console.WriteLine("Subtrção............[2]");
+            Console.WriteLine("Multiplicação.......[3]");
+            Console.WriteLine("Divisão.............[4]");
+            Console.WriteLine("Radiciação..........[5]");
+            Console.WriteLine("Potenciação.........[6]");
+            Console.WriteLine("Sair................[0]");
+
+            opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado Por Usar Nossa Calculadora. Até Mais!");
+                        break;
+                    case 1:
+                        Soma();      
+                        Menu();
+                        break;
+                    case 2:
+                        Subtracao();
+                        Menu();
+                        break;
+                    case 3:
+                        Multiplicacao();
+                        Menu();
+                        break;
+                    case 4:
+                        Divisao();
+                        Menu();
+                        break;
+                    case 5:
+                        Radiciacao();
+                        Menu();
+                        break;
+                    case 6:
+                        Pontenciacao();
+                        Menu();
+                        break;
+                    default:
+                        Console.WriteLine("Digite um valor Valido!");
+                        Menu();
+                        break;
+                }            
+        }
+
+        static void Soma()
+        {
+            double primeiroValor, segundoValor, resultado;
+
+            Console.WriteLine("Digite o Primeiro Valor:");
+            primeiroValor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo Valor:");
+            segundoValor = double.Parse(Console.ReadLine());
+
+            resultado = primeiroValor + segundoValor;
+
+            Console.WriteLine("A Soma do numero {0} mais {1} é igual a {2}", primeiroValor, segundoValor, resultado);
+
+        }
+
+        static void Subtracao()
+        {
+
+            double primeiroValor, segundoValor, resultado;
+
+            Console.WriteLine("Digite o Primeiro Valor:");
+            primeiroValor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo Valor:");
+            segundoValor = double.Parse(Console.ReadLine());
+
+            resultado = primeiroValor - segundoValor;
+
+            Console.WriteLine("A Subtração do numero {0} pelo {1} é igual a {2}", primeiroValor, segundoValor, resultado);
+
+        }
+
+        static void Multiplicacao()
+        {
+
+            double primeiroValor, segundoValor, resultado;
+
+            Console.WriteLine("Digite o Primeiro Valor:");
+            primeiroValor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo Valor:");
+            segundoValor = double.Parse(Console.ReadLine());
+
+            resultado = primeiroValor * segundoValor;
+
+            Console.WriteLine("O Produto do numero {0} pelo {1} é igual a {2}", primeiroValor, segundoValor, resultado);
+        }
+
+        static void Divisao()
+        {
+
+            double primeiroValor, segundoValor, resultado;
+
+            Console.WriteLine("Digite o Primeiro Valor:");
+            primeiroValor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo Valor:");
+            segundoValor = double.Parse(Console.ReadLine());
+
+            resultado = primeiroValor / segundoValor;
+            //double exibir = Math.Round(resultado, 2);
+
+            Console.WriteLine("A Divisão do numero {0} por {1} é igual a {2}", primeiroValor, segundoValor, Math.Round(resultado, 2));
+        }
+
+        static void Radiciacao()
+        {
+
+            double primeiroValor, segundoValor, resultado;
+
+            Console.WriteLine("Digite um valor para saber sua Raiz Quadrada:");
+            primeiroValor = double.Parse(Console.ReadLine());
+
+
+            resultado = Math.Sqrt(primeiroValor);
+
+            Console.WriteLine("A Raiz Quadrada de {0} é {1}", primeiroValor, Math.Round(resultado, 2));
+        }
+
+        static void Pontenciacao()
+        {
+
+            double primeiroValor, segundoValor, resultado;
+
+            Console.WriteLine("Digite o Primeiro Valor para Base:");
+            primeiroValor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Segundo Valor para o Expoente:");
+            segundoValor = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(primeiroValor, segundoValor);
+
+            Console.WriteLine("O numero {0} elevado a {1}ª ponencia é {2}", primeiroValor, segundoValor, resultado);
+        }
+    }
+}
